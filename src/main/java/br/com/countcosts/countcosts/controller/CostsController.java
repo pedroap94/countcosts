3733b31d1a37379a82069c30334743d18b1ca268
Pedro <pedro.pinto@projuris.com.br>
2021-10-12 23:53:13 -0300

    Modified CostsRequest to create conditions in controller input. Added permission to CORS in localhost because I want work in frontend local too

--- src/main/java/br/com/countcosts/countcosts/controller/CostsController.java
+++ src/main/java/br/com/countcosts/countcosts/controller/CostsController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("costs")
 @RequiredArgsConstructor
+@CrossOrigin
 public class CostsController {
     private final CostsService costsService;
 
--- src/main/java/br/com/countcosts/countcosts/dto/CostsRequest.java
+++ src/main/java/br/com/countcosts/countcosts/dto/CostsRequest.java
@@ -1,13 +1,18 @@
 package br.com.countcosts.countcosts.dto;
 
 import lombok.Data;
+import org.springframework.lang.Nullable;
 
+import javax.validation.constraints.Min;
 import java.time.LocalDate;
 
 @Data
 public class CostsRequest {
     private int id;
+    @Nullable
+    @Min(0)
     private Double value;
+    @Nullable
     private LocalDate date;
     private String type;
 }
