94fefaf3e493f0c0a6ef4f6d88964c8e8610cdc6
Pedro <pedro.pinto@projuris.com.br>
2021-10-17 17:27:12 -0300

    Fix human error in MonthCostsController

--- src/main/java/br/com/countcosts/countcosts/controller/MonthCostsController.java
+++ src/main/java/br/com/countcosts/countcosts/controller/MonthCostsController.java
@@ -1,48 +1,6 @@
 package br.com.countcosts.countcosts.controller;
 
-import br.com.countcosts.countcosts.domain.MonthCosts;
-import br.com.countcosts.countcosts.dto.MonthCostsRequest;package br.com.countcosts.countcosts.service;
-
-import br.com.countcosts.countcosts.domain.MonthCosts;
 import br.com.countcosts.countcosts.dto.MonthCostsRequest;
-import br.com.countcosts.countcosts.dto.MonthCostsResponse;
-import br.com.countcosts.countcosts.mapper.MonthCostsMapper;
-import br.com.countcosts.countcosts.repository.MonthCostsRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-
-
-@Service
-@RequiredArgsConstructor
-public class MonthCostsService {
-    private final MonthCostsRepository monthCostsRepository;
-
-    public List<MonthCostsResponse> findValueByMonthAndYear(Integer month, Integer year) {
-        List<MonthCostsResponse> response = new ArrayList<>();
-        for (MonthCosts monthCosts : monthCostsRepository.findByMonthAndYear(month, year)) {
-            MonthCostsResponse monthCostsResponse = MonthCostsMapper.INSTANCE.toMonthCostsResponse(monthCosts);
-            response.add(monthCostsResponse);
-        }
-        return response;
-    }
-
-    public void replaceOrSave(MonthCostsRequest monthCostsRequest) {
-        List<MonthCosts> meet = monthCostsRepository.findByMonthAndYearAndType(monthCostsRequest.getMonth(), monthCostsRequest.getYear(), monthCostsRequest.getType());
-        if (!meet.isEmpty()) {
-            Long idMeet = meet.get(0).getId();
-            MonthCosts monthCosts = MonthCostsMapper.INSTANCE.toMonthCosts(monthCostsRequest);
-            monthCosts.setId(idMeet);
-            monthCostsRepository.save(monthCosts);
-        } else {
-            MonthCosts monthCosts = MonthCostsMapper.INSTANCE.toMonthCosts(monthCostsRequest);
-            monthCostsRepository.save(monthCosts);
-        }
-    }
-}
-
 import br.com.countcosts.countcosts.dto.MonthCostsResponse;
 import br.com.countcosts.countcosts.service.MonthCostsService;
 import lombok.RequiredArgsConstructor;
@@ -70,5 +28,4 @@ public class MonthCostsController {
         monthCostsService.replaceOrSave(monthCostsRequest);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
-
 }
